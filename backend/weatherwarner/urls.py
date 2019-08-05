8d27dfe7b1b8107e6902146b97779c9c48fa2edc
Jake Barber <jake.b.dev@gmail.com>
2019-08-05 10:31:08 +1000

    Remove Sentry debug endpoint

--- backend/weatherwarner/urls.py
+++ backend/weatherwarner/urls.py
@@ -17,9 +17,4 @@ from django.contrib import admin
 from django.urls import path
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-    return division_by_zero
-
-
-urlpatterns = [path("admin/", admin.site.urls), path("sentry-debug/", trigger_error)]
+urlpatterns = [path("admin/", admin.site.urls)]
