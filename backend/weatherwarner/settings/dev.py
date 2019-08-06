f7fdd6d06c4a8d7a9541769383d966eeebae4c50
Jake Barber <jake.b.dev@gmail.com>
2019-08-06 23:29:00 +1000

    Improve dev config for hot reloading

--- backend/weatherwarner/settings/dev.py
+++ backend/weatherwarner/settings/dev.py
@@ -3,5 +3,7 @@ from .base import *  # noqa: F403 F401
 # # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "[::1]"]
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "e80q1y4i3-kq#g+@#%rsfh!(sdg*o6-+%&htc-3i47qcutpare"
